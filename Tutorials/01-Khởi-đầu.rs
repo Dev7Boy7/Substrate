dbee88711a5371971b84d4e7f7075f4f35343c26
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-24 16:55:49 +0700

    Create 01-Khởi-đầu.rs

---
+++ Tutorials/01-Khởi-đầu.rs
@@ -0,0 +1,32 @@
+// Khởi đầu : 
+
+// qua một số minh họa về những hoạt động cơ bản của  Substrate-based blockchain nodes . Gồm 
+
+// cách các note giao tiếp với nhau trong mạng lưới ngang hàng và cách thu thập những thông số hoạt động của node . 
+
+// Nên hoàn thành các hướng dẫn theo thứ tự vì nó là nền tảng từ đơn giản đến phức tạp 
+
+
+// 1. Build a local blockchain 
+
+// chỉ cho bạn cách dể cài đặt và tương tác với node cục bộ trong môi trường phát triển của bạn 
+
+
+// 2. Simulate a network 
+
+// giúp bạn mô phỏng 1 mạng 2 node ( sử dụng các tài khoản định trước)
+
+
+// 3. Add trusted nodes
+
+// trình bày cách tạo keys và chỉ ra các thông số kĩ thuật đặc trưng của chuối dể tạo một mạng lưới nhỏ các node validator tin cậy
+
+
+// 4. Monitor node metrics 
+
+// đưa ra các điểm nổi bật cách mà bạn có thể lấy các lợi thế của the node metrics Substrate 
+
+
+// 5. Upgrade a running node 
+
+// các minh họa về các nâng cấp không cần fork bằng các thay đổi các runtime cho các node Substrate đang chạy
