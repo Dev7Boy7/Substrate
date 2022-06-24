2339e7fc2095a2ce2857505049b8c652fd2796a5
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-24 17:25:11 +0700

    Create 02 -Build-a-local-blockchain.rs

---
+++ Tutorials/02 -Build-a-local-blockchain.rs
@@ -0,0 +1,33 @@
+
+/*  Build a local blockchain
+
+    - Mạng blockchain được tạo từ một mạng lưới máy tính phi tập trung được gọi là các node 
+
+    - Substrate cung cấp một môi trường phát triển linh hoạt , có thể mở rộng cho phép bạn xây dựng một node blockchain tùy chỉnh hoàn toàn theo nhu cầu 
+
+    - Mục tiêu :
+
+        1. Biên dịch the node template  và bắt đầu một blockchain cục bộ dựa trên Substrate 
+
+        2. Cài đặt front - end template để tương tác với node blockchain cục bộ 
+
+        3. Sử dụng front - end template thử gởi giao dịch 
+
+
+    - Cài đặt thì tham khảo ở https://docs.substrate.io/tutorials/get-started/build-local-blockchain/ 
+
+    - Nếu có lỗi hoặc thắc mắc thì tham khảo https://www.youtube.com/watch?v=h5cneMNSPuY&t=1060s của sếp Tú Octopus
+
+    - Nhớ 2 câu lệnh để khởi chạy blockchain cục bộ 
+
+        1. Ở node-template : ./target/release/node-template --dev
+
+        2. Ở front-end template: yarn start
+
+    - Thử sử dụng chức năng transfer 
+
+    - Dừng hoạt động node , quay trở lại Terminal hoặc Ubuntu => Ctrl + C
+
+*/
+
+
