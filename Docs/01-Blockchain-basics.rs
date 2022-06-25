77f44790a850e73c1b1790adb4d46d7472b2b274
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-25 13:00:09 +0700

    Update 01-Blockchain-basics.rs

--- Docs/01-Blockchain-basics.rs
+++ Docs/01-Blockchain-basics.rs
@@ -1,5 +1,16 @@
 /* 
-
+      Blockchain là một sổ cái phi tập trung ghi lại thông tin về một chuỗi các khối .
+      
+      Thông tin chứa trong các blocks là thứ tự các tập hợp lệnh => có thể dẫn tới thay đổi trạng thái .
+      
+      Trong mạng lưới , các máy tính riêng lẻ được gọi là các node - giao tiếp với nhau 
+      => Mạng ngang hàng phi tập trung
+      
+      Không có cơ quan trung ương hoặc bên thứ 3 nào tham gia kiểm soát mạng . 
+      
+      Mỗi node có 1 bản sao của blocks tạo nên canonical chain 
+      
+      
 
 
 */
