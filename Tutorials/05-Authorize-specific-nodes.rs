2e9761be67ec7be0f492a6de6b0b7487c92334ac
TriCao <106253830+Dev7Boy7@users.noreply.github.com>
2022-06-25 13:02:23 +0700

    Update 05-Authorize-specific-nodes.rs

--- Tutorials/05-Authorize-specific-nodes.rs
+++ Tutorials/05-Authorize-specific-nodes.rs
@@ -72,6 +72,24 @@ std = [
 ]
     
 => chạy cargo check -p node-template-runtime => LỖI ............ 
+
+error: failed to select a version for `parity-util-mem`.
+    ... required by package `sp-core v5.0.0 (https://github.com/paritytech/substrate.git?tag=devhub/latest#22d40c76)`
+    ... which satisfies git dependency `sp-core` of package `frame-support v4.0.0-dev (https://github.com/paritytech/substrate.git?tag=devhub/latest#22d40c76)`
+    ... which satisfies git dependency `frame-support` of package `frame-system v4.0.0-dev (https://github.com/paritytech/substrate.git?tag=devhub/latest#22d40c76)`
+    ... which satisfies git dependency `frame-system` of package `pallet-node-authorization v4.0.0-dev (https://github.com/paritytech/substrate.git?tag=devhub/latest#22d40c76)`
+    ... which satisfies git dependency `pallet-node-authorization` of package `node-template-runtime v4.0.0-dev (/mnt/c/Users/trinc/Substrate-tutorial/substrate-node-template/runtime)`
+    ... which satisfies path dependency `node-template-runtime` (locked to 4.0.0-dev) of package `node-template v4.0.0-dev (/mnt/c/Users/trinc/Substrate-tutorial/substrate-node-template/node)`
+versions that meet the requirements `^0.10.2` are: 0.10.2
+
+the package `parity-util-mem` links to the native library `parity-util-mem-ban-duplicates`, but it conflicts with a previous package which links to `parity-util-mem-ban-duplicates` as well:
+package `parity-util-mem v0.11.0`
+    ... which satisfies dependency `parity-util-mem = "^0.11"` (locked to 0.11.0) of package `kvdb v0.11.0`
+    ... which satisfies dependency `kvdb = "^0.11.0"` (locked to 0.11.0) of package `frame-benchmarking-cli v4.0.0-dev (https://github.com/paritytech/substrate.git?branch=polkadot-v0.9.23#6cbe1772)`
+    ... which satisfies git dependency `frame-benchmarking-cli` (locked to 4.0.0-dev) of package `node-template v4.0.0-dev (/mnt/c/Users/trinc/Substrate-tutorial/substrate-node-template/node)`
+Only one package in the dependency graph may specify the same links value. This helps ensure that only one copy of a native library is linked in the final binary. Try to adjust your dependencies so that only one package uses the links ='parity-util-mem' value. For more information, see https://doc.rust-lang.org/cargo/reference/resolver.html#links.
+
+failed to select a version for `parity-util-mem` which could resolve this conflict
     
     
 */
